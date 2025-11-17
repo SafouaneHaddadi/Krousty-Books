7c3931ac0bd3b57f61131f0da5b1c1f5fa3a7b2c
333palmer <romain7re@gmail.com>
2025-11-17 21:21:29 +0100

    Feat: Ajout du endpoint POST /api/books #4

--- backend/src/main/java/com/biblio/backend/controller/BookController.java
+++ backend/src/main/java/com/biblio/backend/controller/BookController.java
@@ -1,12 +1,37 @@
 package com.biblio.backend.controller;
 
+import com.biblio.backend.model.Book; 
+import com.biblio.backend.repository.BookRepository; 
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.http.HttpStatus; 
+import org.springframework.http.ResponseEntity; 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/books")
-
+@CrossOrigin(origins = "http://localhost:4200") 
 public class BookController {
-    
-        // Chacun ajoutera ses endpoints
+
+ 
+
+    @Autowired 
+    private BookRepository bookRepository;
+
+ 
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        try {
+            
+            Book newBook = bookRepository.save(book);
+            
+      
+            return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+        } catch (Exception e) {
+           
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
     
 }
