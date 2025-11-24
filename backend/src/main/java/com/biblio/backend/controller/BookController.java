2737ef0a2cf9b8899995c50bd599ab1be2920057
SafouaneHaddadi <145685123+SafouaneHaddadi@users.noreply.github.com>
2025-11-24 16:51:12 +0100

    US1: Implémentation de la liste des livres (#8)
    * feat: add GET /api/books endpoint (#3)
    
    * feat(backend): add GET /api/books endpoint (#3)
    
    * test(backend): add unit tests for book listing feature (#3)
    
    * fix(backend): enable CORS for frontend access #3
    
    * build(frontend): fix Docker build permissions and add Bootstrap (#3)
    
    * build: add Bootstrap CSS framework (#3)
    
    * feat(frontend): implement book listing feature (#3)
    
    * config: setup Angular application (#3)
    
    * chore: update configuration files (#3)
    
    * test: fix tests with H2 profile and application-test.yml (#3)
    
    * style(frontend): improve book image size and card layout (#3)
    
    * Enable hot‑reload, and update book‑list component html (#3)
    
    * Fix(frontend):  title truncation (#3)
    
    * feat(ci): add GitHub Actions workflow for backend (#3)
    
    * feat: add DevContainer configuration (#3)
    
    * fix: backend CORS configuration and permissions (#3)
    
    * fix: frontend DevContainer configuration (#3)
    
    * feat (frontend): add edit and delete buttons for books (#3)
    
    * chore: update docker-compose for production only
    
    * chore: clean up frontend Dockerfile for production only (#3)

--- backend/src/main/java/com/biblio/backend/controller/BookController.java
+++ backend/src/main/java/com/biblio/backend/controller/BookController.java
@@ -1,12 +1,24 @@
 package com.biblio.backend.controller;
 
+import com.biblio.backend.model.Book;
+import com.biblio.backend.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/books")
+
+@RestController 
+@RequestMapping("/api/books") 
 
 public class BookController {
-    
-        // Chacun ajoutera ses endpoints
-    
+
+    @Autowired
+    private BookService bookService; 
+
+    @GetMapping //GET /api/books
+    @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8082"})  //Sans cette config, le navigateur bloque les appels API entre le frontend et le backend 
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks(); // délégue la logique métier au service
+    }
+
 }
--- backend/src/main/java/com/biblio/backend/service/BookService.java
+++ backend/src/main/java/com/biblio/backend/service/BookService.java
@@ -1,6 +1,19 @@
 package com.biblio.backend.service;
 
+import com.biblio.backend.model.Book;
+import com.biblio.backend.repository.BookRepository; //BookRepository : Interface pour communiquer avec la base de données
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class BookService {
-        // Chacun implémentera ses méthodes
+
+@Service 
+public class BookService { 
+
+    @Autowired 
+    private BookRepository bookRepository; 
+
+    public List<Book> getAllBooks() {
+       return bookRepository.findAll(); // appel à JPA qui génère le sql
+}
 }
