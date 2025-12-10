9a7e690abae5b998c4a2964008f817de55fa863d
Jereminhoo <jeremy.muanza@condorcet.be>
2025-12-10 12:33:04 +0100

    feat: add DELETE /api/books/{id} endpoint (#4) (#9)

--- backend/src/main/java/com/biblio/backend/service/BookService.java
+++ backend/src/main/java/com/biblio/backend/service/BookService.java
@@ -3,17 +3,26 @@ package com.biblio.backend.service;
 import com.biblio.backend.model.Book;
 import com.biblio.backend.repository.BookRepository; //BookRepository : Interface pour communiquer avec la base de données
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
+public class BookService {
 
-@Service 
-public class BookService { 
+    private final BookRepository bookRepository;
 
-    @Autowired 
-    private BookRepository bookRepository; 
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     public List<Book> getAllBooks() {
-       return bookRepository.findAll(); // appel à JPA qui génère le sql
-}
+        return bookRepository.findAll(); // appel à JPA qui génère le sql
+    }
+
+    public void deleteBook(Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new RuntimeException("Livre introuvable");
+        }
+
+        bookRepository.deleteById(id);
+    }
 }
