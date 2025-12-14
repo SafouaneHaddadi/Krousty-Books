44d0ca1ee28ceb3931ba5ad43b82f4dcab663f34
Romain Tiguenitine <romain7re@gmail.com>
2025-12-14 18:36:21 +0100

    US4: Implémentation de l'ajout d'un livre (Backend) (#7)
    * Feat: Ajout du endpoint POST /api/books #4
    
    * Fix: Mise à jour des tests (Mock BookRepository)

--- backend/src/main/java/com/biblio/backend/controller/BookController.java
+++ backend/src/main/java/com/biblio/backend/controller/BookController.java
@@ -1,41 +1,46 @@
 package com.biblio.backend.controller;
 
 import com.biblio.backend.model.Book;
+import com.biblio.backend.repository.BookRepository;
 import com.biblio.backend.service.BookService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
-
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8082"})
 public class BookController {
 
     @Autowired
+    private BookRepository bookRepository;
+    
+    @Autowired
     private BookService bookService;
 
-    @GetMapping //GET /api/books
-    @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8082"})  //Sans cette config, le navigateur bloque les appels API entre le frontend et le backend
+    @GetMapping
     public List<Book> getAllBooks() {
-        return bookService.getAllBooks(); // délégue la logique métier au service
+        return bookService.getAllBooks();
+    }
+  
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        try {
+            Book newBook = bookRepository.save(book);
+            return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    // Endpoint DELETE /api/books/{id}
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBook(@PathVariable Long id) { // renvoit un code pour montrer le resultat de la requete http
-
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         try {
-            // On appelle le service pour supprimer le livre
             bookService.deleteBook(id);
-
-            // Si tout se passe bien → retour 204 (aucun contenu)
             return ResponseEntity.noContent().build();
-
         } catch (RuntimeException e) {
-            // Si le livre n'existe pas → on renvoie 404 (Not Found)
             return ResponseEntity.notFound().build();
         }
     }
