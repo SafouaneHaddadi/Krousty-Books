7bf7276632311f18ee06e38afb0c1e466761ef8f
SafouaneHaddadi <safouane.haddadi@condorcet.be>
2025-12-16 20:54:34 +0100

    feat(reviews): add  my-reviews and user endpoints

--- backend/src/main/java/com/biblio/backend/service/ReviewService.java
+++ backend/src/main/java/com/biblio/backend/service/ReviewService.java
@@ -1,14 +1,20 @@
 package com.biblio.backend.service;
 
 import com.biblio.backend.model.Book;
+import com.biblio.backend.model.Borrow;
 import com.biblio.backend.model.Review;
 import com.biblio.backend.model.User;
 import com.biblio.backend.repository.BookRepository;
+import com.biblio.backend.repository.BorrowRepository;
 import com.biblio.backend.repository.ReviewRepository;
 import com.biblio.backend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 @Transactional
@@ -16,30 +22,25 @@ public class ReviewService {
 
     @Autowired
     private ReviewRepository reviewRepository;
-    
     @Autowired
     private BookRepository bookRepository;
-    
     @Autowired
     private UserRepository userRepository;
 
     public Review addReview(Long bookId, int rating, String comment, String username) {
 
-        // Validation de la note
         if (rating < 1 || rating > 5) {
             throw new IllegalArgumentException("La note doit être comprise entre 1 et 5.");
         }
 
-        // Récupération du livre
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new RuntimeException("Livre introuvable avec l'ID " + bookId));
 
-        
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new RuntimeException("Utilisateur introuvable"));
 
-        // Création et sauvegarde
         Review review = new Review(rating, comment, book, user);
+
         return reviewRepository.save(review);
     }
 }
