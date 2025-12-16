4ea97e017bd5c7400a042ca31f5f77c8538d4daa
SafouaneHaddadi <145685123+SafouaneHaddadi@users.noreply.github.com>
2025-12-16 21:27:15 +0100

    Revert "feat(reviews): gestion complète des avis avec sécurité et endpoints d…"
    This reverts commit 06577604d39acbb8a62015da27b53872f67da036.

--- backend/src/main/java/com/biblio/backend/BackendApplication.java
+++ backend/src/main/java/com/biblio/backend/BackendApplication.java
@@ -2,29 +2,12 @@ package com.biblio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-        //BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        //String hash1 = encoder.encode("...");
-        //System.out.println(hash1);
 	}
-
-	/*@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "http://localhost:8082")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
-    } */
 
 }
--- backend/src/main/java/com/biblio/backend/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biblio.backend.repository;
-
-import com.biblio.backend.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
