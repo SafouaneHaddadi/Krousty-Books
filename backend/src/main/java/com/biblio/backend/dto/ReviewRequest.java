ff82b013d4ba13e4f31ca96d1de27931e0ba3a94
333palmer <romain7re@gmail.com>
2025-12-16 20:04:58 +0100

    Fix: fix error in the Reviews files

--- backend/src/main/java/com/biblio/backend/dto/ReviewRequest.java
+++ backend/src/main/java/com/biblio/backend/dto/ReviewRequest.java
@@ -6,12 +6,13 @@ public class ReviewRequest {
     private int rating;
     private String comment; 
 
+    // Getters et Setters
     public int getRating() { return rating; }
     public void setRating(int rating) { this.rating = rating; }
-
+    
     public String getComment() { return comment; }
     public void setComment(String comment) { this.comment = comment; }
-
+    
     public Long getBookId() { return bookId; }
     public void setBookId(Long bookId) { this.bookId = bookId; }
 }
